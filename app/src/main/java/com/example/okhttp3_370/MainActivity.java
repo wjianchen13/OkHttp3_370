f496ed59a0ca467e0ac71745af2f034706fd8755
cwj <282330332@qq.com>
2024-10-30 15:34:00 +0800

    初次提交

---
+++ app/src/main/java/com/example/okhttp3_370/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.okhttp3_370;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        OkHttpClient client = new OkHttpClient();
+
+//        Request request = new Request.Builder()
+//                .url(url)
+//                .build();
+//
+//        Response response = client.newCall(request).execute();
+//        return response.body().string();
+
+    }
+}
